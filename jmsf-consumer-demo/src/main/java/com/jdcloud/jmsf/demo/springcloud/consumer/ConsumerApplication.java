aabae1478f7137fa2ce4cb325554b70818bb41ab
Zhiguo.Chen <chenzhiguo@live.com>
2022-09-01 17:30:39 +0800

    Add comment and upgrade jmsf version to 1.3.6

--- jmsf-consumer-demo/src/main/java/com/jdcloud/jmsf/demo/springcloud/consumer/ConsumerApplication.java
+++ jmsf-consumer-demo/src/main/java/com/jdcloud/jmsf/demo/springcloud/consumer/ConsumerApplication.java
@@ -18,12 +18,13 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class ConsumerApplication {
 
     public static void main(String[] args) {
-        //模拟设置环境变量
+        // 相关解释参照jmsf-provider-demo的main方法
         // System.setProperty("JMESH_SERVICE_NAME", "CustomConsumerService");
         // System.setProperty("JMESH_REGISTRY_TOKEN", "6fca9ebf-ab1d-8023-9348-e6e12649968f");
 
         System.setProperty("JMESH_SERVICE_NAMESPACE", "default");
         System.setProperty("JMESH_SERVICE_APP", "jmsf-consumer-demo");
+        System.setProperty("JMESH_SERVICE_ID", "jmsf-provider-demo");
         System.setProperty("JMESH_SERVICE_VERSION", "v1.0");
         System.setProperty("JMESH_SERVICE_GROUP", "group1");
         System.setProperty("JMESH_SERVICE_CLUSTER", "cluster-dev");
@@ -32,6 +33,8 @@ public class ConsumerApplication {
         System.setProperty("JMESH_MESH_GROUP", "mesh01");
         System.setProperty("JMESH_SERVICE_DEPLOYMENT", "dep1");
         System.setProperty("JMESH_ZONE", "zone1");
+
+        // 忽略下面这个环境变量
         System.setProperty("PROVIDER_NAME", "sc-jmsf-provider");
         SpringApplication.run(ConsumerApplication.class, args);
     }
--- jmsf-provider-demo/src/main/java/com/jdcloud/jmsf/demo/springcloud/provider/ProviderApplication.java
+++ jmsf-provider-demo/src/main/java/com/jdcloud/jmsf/demo/springcloud/provider/ProviderApplication.java
@@ -12,19 +12,25 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProviderApplication {
 
     public static void main(String[] args) {
-        //模拟设置环境变量
+        // 注意：下面的这些环境变量再生产环境理论上是不需要的，JMSF会自动注入这些环境变量，也就是打生产包时需要注释掉。
+        // 因为开发项目时，本期启动时不会被自动注入所以需要通过下方代码模拟设置环境变量
         // System.setProperty("JMESH_SERVICE_NAME", "CustomProviderService");
         // System.setProperty("JMESH_REGISTRY_TOKEN", "6fca9ebf-ab1d-8023-9348-e6e12649968f");
 
+        // 需同JMSF控制台显示的命名空间一致
         System.setProperty("JMESH_SERVICE_NAMESPACE", "default");
+        // 需同JMSF控制台创建的服务名一致
         System.setProperty("JMESH_SERVICE_APP", "jmsf-provider-demo");
+        System.setProperty("JMESH_SERVICE_ID", "jmsf-provider-demo");
         System.setProperty("JMESH_SERVICE_VERSION", "v1.0");
         System.setProperty("JMESH_SERVICE_GROUP", "group1");
+        // 需同JMSF控制台显示的K8s集群一致
         System.setProperty("JMESH_SERVICE_CLUSTER", "cluster-dev");
+        // 注册中心地址 注意：如果使用K8s Service或者F5等负载均衡，需要运维人员配置负载均衡策略为IP保持，否则可能会在心跳检测时输出异常信息
         System.setProperty("JMESH_REGISTRY_HOST", "127.0.0.1");
+        // 注册中心端口
         System.setProperty("JMESH_REGISTRY_PORT", "8500");
-        // System.setProperty("JMESH_REGISTRY_HOST", "consul.test.tpaas");
-        // System.setProperty("JMESH_REGISTRY_PORT", "80");
+        // 需同JMSF控制台显示的网格名称一致
         System.setProperty("JMESH_MESH_GROUP", "mesh01");
         System.setProperty("JMESH_SERVICE_DEPLOYMENT", "dep1");
         System.setProperty("JMESH_ZONE", "zone1");
