5a81b8ba4c436932471155170c1946920d7bec8c
Zhiguo.Chen <chenzhiguo@live.com>
2022-09-01 18:18:10 +0800

    Add comment

--- jmsf-provider-demo/src/main/java/com/jdcloud/jmsf/demo/springcloud/provider/ProviderApplication.java
+++ jmsf-provider-demo/src/main/java/com/jdcloud/jmsf/demo/springcloud/provider/ProviderApplication.java
@@ -14,7 +14,6 @@ public class ProviderApplication {
     public static void main(String[] args) {
         // 注意：下面的这些环境变量再生产环境理论上是不需要的，JMSF会自动注入这些环境变量，也就是打生产包时需要注释掉。
         // 因为开发项目时，本期启动时不会被自动注入所以需要通过下方代码模拟设置环境变量
-        // System.setProperty("JMESH_SERVICE_NAME", "CustomProviderService");
 
         // 正常情况下consul都会开启token验证，所以这个环境变量也需要。token在jmsf控制台上的分组管理-进入具体某个分组可以看到"注册中心token"
         // System.setProperty("JMESH_REGISTRY_TOKEN", "6fca9ebf-ab1d-8023-9348-e6e12649968f");
@@ -23,8 +22,10 @@ public class ProviderApplication {
         System.setProperty("JMESH_SERVICE_NAMESPACE", "default");
         // 需同JMSF控制台创建的服务名一致
         System.setProperty("JMESH_SERVICE_APP", "jmsf-provider-demo");
+        // 需同JMSF控制台创建的服务名一致
         System.setProperty("JMESH_SERVICE_ID", "jmsf-provider-demo");
         System.setProperty("JMESH_SERVICE_VERSION", "v1.0");
+        // 分组，开发过程中不用在意，可随意写，服务治理中会用到
         System.setProperty("JMESH_SERVICE_GROUP", "group1");
         // 需同JMSF控制台显示的K8s集群一致
         System.setProperty("JMESH_SERVICE_CLUSTER", "cluster-dev");
@@ -34,8 +35,11 @@ public class ProviderApplication {
         System.setProperty("JMESH_REGISTRY_PORT", "8500");
         // 需同JMSF控制台显示的网格名称一致
         System.setProperty("JMESH_MESH_GROUP", "mesh01");
+        // 部署名，开发过程中不用在意，可随意写，服务治理中会用到
         System.setProperty("JMESH_SERVICE_DEPLOYMENT", "dep1");
+        // 分区名，开发过程中不用在意，可随意写，服务治理中会用到
         System.setProperty("JMESH_ZONE", "zone1");
+        
         SpringApplication.run(ProviderApplication.class, args);
     }
 
